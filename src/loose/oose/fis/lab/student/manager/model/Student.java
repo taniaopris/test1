a24bf3fa1cb08f32681782e3f195bb852a998d87
AltGeluPOPA <gelu815@gmail.com>
2019-04-08 11:05:53 +0300

    feature added

---
+++ src/loose/oose/fis/lab/student/manager/model/Student.java
@@ -0,0 +1,8 @@
+package loose.oose.fis.lab.student.manager.model;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+}
